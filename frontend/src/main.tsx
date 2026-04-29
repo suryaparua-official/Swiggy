17b3c13266f28f109dcac51de903d1d8dfa32ae3
suryaparua-official <surya.exce@gmail.com>
2026-04-29 17:42:53 +0530

    fix some bugs

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,16 +7,22 @@ import { AppProvider } from "./context/AppContext.tsx";
 import "leaflet/dist/leaflet.css";
 import { SocketProvider } from "./context/SocketContext.tsx";
 
-export const authService = "http://localhost:5000";
-export const restaurantService = "http://localhost:5001";
-export const utilsService = "http://localhost:5002";
-export const realtimeService = "http://localhost:5004";
-export const riderService = "http://localhost:5005";
-export const adminService = "http://localhost:5006";
+export const authService =
+  import.meta.env.VITE_AUTH_URL ?? "http://localhost:5000";
+export const restaurantService =
+  import.meta.env.VITE_RESTAURANT_URL ?? "http://localhost:5001";
+export const utilsService =
+  import.meta.env.VITE_UTILS_URL ?? "http://localhost:5002";
+export const realtimeService =
+  import.meta.env.VITE_REALTIME_URL ?? "http://localhost:5004";
+export const riderService =
+  import.meta.env.VITE_RIDER_URL ?? "http://localhost:5005";
+export const adminService =
+  import.meta.env.VITE_ADMIN_URL ?? "http://localhost:5006";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId="491400024355-dhd50s4e1iejd2l9f8lhpmahr1iljfq8.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <AppProvider>
         <SocketProvider>
           <App />
